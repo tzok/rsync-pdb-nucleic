a1e8b932a8edba550e313d6dff85126186858327
Tomasz Zok <tomasz.zok@gmail.com>
2026-02-02 15:45:13 +0100

    refactor: Use temp file and --delete-excluded for rsync includes

--- rsync-pdb-nucleic.sh
+++ rsync-pdb-nucleic.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
+set -x
+
 BACKUP_DIR=/home/tzok/rsync-pdb-nucleic
 
 mkdir \
@@ -13,13 +15,16 @@ rsync \
 	rsync.ebi.ac.uk::pub/databases/pdb/derived_data/pdb_entry_type.txt \
 	${BACKUP_DIR}/derived_data/pdb_entry_type.txt
 
-pdbids=($(cat ${BACKUP_DIR}/derived_data/pdb_entry_type.txt | awk '$2 == "nuc" || $2 == "prot-nuc" { print $1 }'))
+pdbids_file=$(mktemp)
+cat ${BACKUP_DIR}/derived_data/pdb_entry_type.txt | awk '$2 == "nuc" || $2 == "prot-nuc" { print $1 "*" }' > "$pdbids_file"
 
 rsync \
 	--archive \
 	--progress \
+	--stats \
 	--delete \
-	--include-from=<(printf "%s*\n" ${pdbids[@]}) \
+	--delete-excluded \
+	--include-from="$pdbids_file" \
 	--include='*/' \
 	--exclude='*' \
 	rsync.ebi.ac.uk::pub/databases/pdb/data/assemblies/mmCIF/divided/ \
@@ -28,9 +33,13 @@ rsync \
 rsync \
 	--archive \
 	--progress \
+	--stats \
 	--delete \
-	--include-from=<(printf "%s*\n" ${pdbids[@]}) \
+	--delete-excluded \
+	--include-from="$pdbids_file" \
 	--include='*/' \
 	--exclude='*' \
 	rsync.ebi.ac.uk::pub/databases/pdb/data/structures/divided/mmCIF/ \
 	${BACKUP_DIR}/data/structures/divided/mmCIF/
+
+rm "$pdbids_file"
